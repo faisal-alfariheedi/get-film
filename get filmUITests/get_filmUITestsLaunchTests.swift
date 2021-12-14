57f688f95fcf43b3a662b576ad3521f42c87f308
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-14 12:27:12 +0300

    Initial Commit

---
+++ get filmUITests/get_filmUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  get_filmUITestsLaunchTests.swift
+//  get filmUITests
+//
+//  Created by faisal on 10/05/1443 AH.
+//
+
+import XCTest
+
+class get_filmUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
